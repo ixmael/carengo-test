bcf4e3ed7d13748c3935395d7ea693d24ad8317d
Ismael Ramos Merlos <iramosmerlos@outlook.com>
2017-11-21 23:49:52 -0600

    Add template engine and configuration enviroment

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,35 @@
+import path from 'path';
+import config from 'config';
 import express from 'express';
+import atpl from 'atpl';
 
-const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const staticPath = path.join(process.env.NODE_PATH, 'static');
+const templatesPath = path.join(process.env.NODE_PATH, 'templates');
 
-app.listen(3030, () => console.log('Example app listening on port 3030!'))
+const app = express();
+
+// Log the requests
+//app.use(logger('dev'));
+
+// Serve static files
+app.use(express.static(staticPath));
+
+//app.set("view engine", "pug");
+app.engine('html', atpl.__express)
+app.set('view engine', 'html');
+app.set('views', path.join(templatesPath));
+
+app.get('/productos', function(request, response) {
+    const page = request.query.page;
+    response.json({test: 'test'});
+});
+
+app.get('/', function(request, response) {
+    response.render("index", { test: 'probando :3'});
+});
+
+// Fire it up!
+const port = config.get('port');
+app.listen(port);
+console.log(`Listening on port ${port}`);
